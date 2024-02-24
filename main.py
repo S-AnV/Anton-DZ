96ff44451352c64d5f90fab9c62f8324a845a2f5
user <antonsfv@gmail.com>
2024-02-24 19:43:59 +0700

    Домашнее задание по теме "Блокировки потоков для доступа к общим данным"

--- main.py
+++ main.py
@@ -10,45 +10,35 @@
 # Ожидается создание двух потоков, один для пополнения счета, другой для снятия денег.
 # Используйте with (lock object): в начале каждого метода, чтобы использовать блокировку
 
-import random
-from collections import defaultdict
 import threading
 
+class BankAccount():
 
-class BankAccount(threading.Thread):
+    def __init__(self):
+        self.balance = 1000
+        self.balance_lock = threading.Lock()
 
-    def __init__(self, lock, deposit, withdraw, account=1000, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.deposit = deposit
-        self.withdraw = withdraw
-        self.account = account
-        self.account_lock = lock
+    def deposit(self, num):
+        with self.balance_lock:
+            self.balance += num
+        print(f'Deposited {num}, new balance is {self.balance}')
 
-    def deposit_task(self, account, amount):  # счет, количество
-        for _ in range(5):
-            account.deposit(amount)
-            with self.account_lock:
-                self.account += amount
-            print(f'Deposited {amount}, new balance is {self.account}')
-
-    def withdraw_task(self, account, amount):
-        for _ in range(5):
-            account.withdraw(amount)
-            with self.account_lock:
-                self.account -= amount
-
-            print(f'Withdrew {amount}, new balance is {self.account}')
-            account = BankAccount()
+    def withdraw(self, num):
+        with self.balance_lock:
+            self.balance -= num
+        print(f'Withdrew {num}, new balance is {self.balance}')
 
 
-account = BankAccount
-# account.deposit_task()
-# deposit_task = BankAccount.deposit_task
-# withdraw_task = BankAccount.withdraw_task
+def deposit_task(account, amount):
+    for _ in range(5):
+        account.deposit(amount)
 
-# deposit_thread = BankAccount.deposit_task(1000, 100)
-#     account = 1000
-lock = threading.Lock()
+def withdraw_task(account, amount):
+    for _ in range(5):
+        account.withdraw(amount)
+
+account = BankAccount()
+
 deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
 withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
 
@@ -57,43 +47,3 @@ withdraw_thread.start()
 
 deposit_thread.join()
 withdraw_thread.join()
-
-# Вывод в консоль:
-# Deposited 100, new balance is 1100
-# Deposited 100, new balance is 1200
-# Deposited 100, new balance is 1300
-# Deposited 100, new balance is 1400
-# Deposited 100, new balance is 1500
-# Withdrew 150, new balance is 1350
-# Withdrew 150, new balance is 1200
-# Withdrew 150, new balance is 1050
-# Withdrew 150, new balance is 900
-# Withdrew 150, new balance is 750
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
