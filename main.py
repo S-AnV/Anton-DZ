6c08db9651c25d4a13a802ec07424dde4d5efb59
user <antonsfv@gmail.com>
2024-02-24 12:22:47 +0700

    Домашнее задание по теме "Блокировки потоков для доступа к общим данным"

--- main.py
+++ main.py
@@ -1,36 +1,99 @@
-import time
-from threading import Thread
+# Задание:
+# Реализуйте программу, которая имитирует доступ к общему ресурсу с использованием механизма блокировки потоков.
+
+# Класс BankAccount должен отражать банковский счет с балансом и методами для пополнения и снятия денег.
+# Необходимо использовать механизм блокировки, чтобы избежать проблемы гонок (race conditions)
+# при модификации общего ресурса.
+
+# Примечание:
+# Используйте класс Lock из модуля threading для блокировки доступа к общему ресурсу.
+# Ожидается создание двух потоков, один для пополнения счета, другой для снятия денег.
+# Используйте with (lock object): в начале каждого метода, чтобы использовать блокировку
+
+import random
+from collections import defaultdict
+import threading
 
 
-class Knight(Thread):
-    def __init__(self, name, skill, *args, **kwargs):
+class BankAccount(threading.Thread):
+
+    def __init__(self, lock, deposit, withdraw, account=1000, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = name
-        self.skill = skill
-        self.enemies = 100
-        self.day = 0
+        self.deposit = deposit
+        self.withdraw = withdraw
+        self.account = account
+        self.account_lock = lock
 
-    def run(self):
-        for skill in range(self.skill):
-            self.day += 1
-            self.enemies -= self.skill
-            print(f'{self.name}, сражается {self.day} день(дня)..., осталось {self.enemies} воинов', flush=True)
-            time.sleep(1)
-            if self.enemies == 0:
-                print(f'{self.name} одержал победу спустя {self.day} дней!', flush=True)
-                return self.run
+    def deposit_task(self, account, amount):  # счет, количество
+        for _ in range(5):
+            account.deposit(amount)
+            with self.account_lock:
+                self.account += amount
+            print(f'Deposited {amount}, new balance is {self.account}')
+
+    def withdraw_task(self, account, amount):
+        for _ in range(5):
+            account.withdraw(amount)
+            with self.account_lock:
+                self.account -= amount
+
+            print(f'Withdrew {amount}, new balance is {self.account}')
+            account = BankAccount()
 
 
-knight1 = Knight("Sir Lancelot", 10)  # Низкий уровень умения
-knight2 = Knight("Sir Galahad", 20)  # Высокий уровень умения
+account = BankAccount
+# account.deposit_task()
+# deposit_task = BankAccount.deposit_task
+# withdraw_task = BankAccount.withdraw_task
 
-print('Sir Lancelot, на нас напали')
-print('Sir Galahad, на нас напали')
+# deposit_thread = BankAccount.deposit_task(1000, 100)
+#     account = 1000
+lock = threading.Lock()
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
 
-knight1.start()
-knight2.start()
+deposit_thread.start()
+withdraw_thread.start()
 
-knight1.join()
-knight2.join()
+deposit_thread.join()
+withdraw_thread.join()
 
-print('Все битвы закончились!')
+# Вывод в консоль:
+# Deposited 100, new balance is 1100
+# Deposited 100, new balance is 1200
+# Deposited 100, new balance is 1300
+# Deposited 100, new balance is 1400
+# Deposited 100, new balance is 1500
+# Withdrew 150, new balance is 1350
+# Withdrew 150, new balance is 1200
+# Withdrew 150, new balance is 1050
+# Withdrew 150, new balance is 900
+# Withdrew 150, new balance is 750
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
