f810a348f57a4b2b1a39dea652ff89050ff36bb8
user <antonsfv@gmail.com>
2024-02-27 21:42:20 +0700

    Домашнее задание по теме "Очереди для обмена данными между потоками."

--- probe.py
+++ probe.py
@@ -1,52 +1,83 @@
-# Задание:
-# Реализуйте программу, которая имитирует доступ к общему ресурсу с использованием механизма блокировки потоков.
-
-# Класс BankAccount должен отражать банковский счет с балансом и методами для пополнения и снятия денег.
-# Необходимо использовать механизм блокировки, чтобы избежать проблемы гонок (race conditions)
-# при модификации общего ресурса.
-
-# Примечание:
-# Используйте класс Lock из модуля threading для блокировки доступа к общему ресурсу.
-# Ожидается создание двух потоков, один для пополнения счета, другой для снятия денег.
-# Используйте with (lock object): в начале каждого метода, чтобы использовать блокировку
-
+import time
+import queue
 import threading
 
-class BankAccount():
-
-    def __init__(self):
-        self.balance = 1000
-        self.balance_lock = threading.Lock()
-
-    def deposit(self, num):
-        with self.balance_lock:
-            self.balance += num
-        print(f'Deposited {num}, new balance is {self.balance}')
-
-    def withdraw(self, num):
-        with self.balance_lock:
-            self.balance -= num
-        print(f'Withdrew {num}, new balance is {self.balance}')
+class Table():
+    def __init__(self, number, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.number = number
+        self.is_busy = False
 
 
-def deposit_task(account, amount):
-    for _ in range(5):
-        account.deposit(amount)
 
-def withdraw_task(account, amount):
-    for _ in range(5):
-        account.withdraw(amount)
+class Cafe():
+    def __init__(self, tables, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.customers = queue.Queue() # очередь посетителей
+        self.tables = tables # список столов (поступает из вне)
+        self.customer_threads = []
 
-account = BankAccount()
+    def customer_arrival(self): # моделирует приход посетителя(каждую секунду)
+         for customer in range(1, 21):
+            print(f'Посетитель номер {customer} прибыл.', flush=True)
+            self.serve_customer(customer)
+            time.sleep(1)
 
-deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
-withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+    def serve_customer(self, customer):
+        table_found = False
+        for table in self.tables:
+            if table.is_busy == False:
+                print(f'Посетитель номер {customer} сел за стол {table.number}.', flush=True)
+                table.is_busy = True
+                customer_task = Customer(customer, table, self.customers, self)
+                customer_task.start() # запускаем поток, начинаем обслуживание посетителя
+                self.customer_threads.append(customer_task)
+                table_found = True
+                break
 
-deposit_thread.start()
-withdraw_thread.start()
+        if not table_found:
+            print(f'Посетитель номер {customer} ожидает свободный стол.', flush=True)
+            self.customers.put(customer)
 
-deposit_thread.join()
-withdraw_thread.join()
+                # customer_task.join()
+
+class Customer(threading.Thread): # класс (поток) посетителя. Запускается, если есть свободные столы.
+    def __init__(self, id, table, queue, cafe):
+        super().__init__()
+        self.customers = queue
+        self.id = id
+        self.table = table
+        self.cafe = cafe
+
+    def run(self):
+        time.sleep(5)
+        print(f'Посетитель номер {self.id} покушал и ушел.', flush=True)
+        self.table.is_busy = False
+        if not self.customers.empty():
+            customer_next = self.customers.get()
+            self.cafe.serve_customer(customer_next)
+
+
+# Создаем столики в кафе
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
+
+# Инициализируем кафе
+cafe = Cafe(tables)
+
+# Запускаем поток для прибытия посетителей
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
+
+# Ожидаем завершения работы прибытия посетителей
+customer_arrival_thread.join()
+
+for i in cafe.customer_threads:
+    i.join()
+
+
 
 
 
