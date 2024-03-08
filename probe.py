2a4897aef10dac169d9c4b754926bea5928c98c2
user <antonsfv@gmail.com>
2024-03-08 19:37:26 +0700

    Домашнее задание по теме "Обзор сторонних библиотек Python"

--- probe.py
+++ probe.py
@@ -1,37 +1,53 @@
-from multiprocessing import Process, Pipe, Queue
-from queue import Empty
+from PIL import Image, ImageOps
+
+"""Изменение размера"""
+image = Image.open("osen-park-skameika-autumn-fall-colors-park.jpg")
+new_size = (100, 100)
+
+resized_image = image.resize(new_size)
+resized_image.save("resized_osen-park-skameika-autumn-fall-colors-park.jpg")
 
 
-class WarehouseManager(Process):
 
-    def __init__(self, data=None):
-        super().__init__()
-        if data is None:
-            data = {}
-        self.data = data
+"""Преобразование изображения в черно-белое"""
+image = Image.open("osen-park-skameika-autumn-fall-colors-park.jpg")
 
-    def process_request(self, request):
-        if request[1] == 'receipt':
-            self.data[request[0]] = request[2]
-        elif request[1] == 'shipment':
-            if request[2] > 0:
-                self.data[request[0]] -= request[2]
-
-    def run(self, requests):
-        for request in requests:
-            self.process_request(request)
+grayscale_image = ImageOps.grayscale(image)
+grayscale_image.save("grayscale_osen-park-skameika-autumn-fall-colors-park.jpg")
 
 
-if __name__ == '__main__':
-    manager = WarehouseManager()
 
-    requests = [
-        ("product1", "receipt", 100),
-        ("product2", "receipt", 150),
-        ("product1", "shipment", 30),
-        ("product3", "receipt", 200),
-        ("product2", "shipment", 50)
-    ]
 
-    manager.run(requests)
-    print(manager.data)
+
+import requests
+
+r = requests.get('https://ya.ru/')
+print(r)
+
+
+
+
+import numpy as np
+a = np.array([20, 30, 40, 50])
+b = np.arange(1, 5)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+phi = np.linspace(0, 2.*np.pi, 100)
+plt.plot(phi, np.sin(phi))
+plt.plot(phi, np.cos(phi))
+
+plt.show()
+plt.close()
