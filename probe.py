76a83cdf68abe0b64bd19c43dba0bd8214e21252
user <antonsfv@gmail.com>
2024-04-07 20:33:35 +0700

    Домашнее задание по теме "Дата и время"

--- probe.py
+++ probe.py
@@ -1,53 +1,50 @@
-from PIL import Image, ImageOps
-
-"""Изменение размера"""
-image = Image.open("osen-park-skameika-autumn-fall-colors-park.jpg")
-new_size = (100, 100)
-
-resized_image = image.resize(new_size)
-resized_image.save("resized_osen-park-skameika-autumn-fall-colors-park.jpg")
+from datetime import datetime
 
 
+class SuperDate(datetime):
+    def __init__(self, year, month, day, hour):
+        self._year = year
+        self._month = month
+        self._day = day
+        self._hour = hour
+    def get_season(self):
+        """
+        возвращает сезон года (Summer, Autumn, Winter, Spring)
+        :return:
+        """
+        if 1 <= self._month <= 12:
+            if 3 <= self._month <= 5:
+                return 'Spring'
+            elif 6 <= self._month <= 8:
+                return 'Summer'
+            elif 9 <= self._month <= 11:
+                return 'Autumn'
+            else:
+                return 'Winter'
+        else:
+            return 'Введен некорректный номер месяца'
 
-"""Преобразование изображения в черно-белое"""
-image = Image.open("osen-park-skameika-autumn-fall-colors-park.jpg")
+    def get_time_of_day(self):
+        """
+        возвращает время суток (Morning: 6-12; Day: 12-18, Evening: 18-0, Night: 0-6)
+        :return:
+        """
+        if 6 <= self._hour <= 11:
+            return 'Morning'
+        elif 12 <= self._hour <= 17:
+            return 'Day'
+        elif 18 <= self._hour <= 23:
+            return 'Evening'
+        elif 0 <= self._hour <= 5:
+            return 'Night'
+        else:
+            return 'Введено некорректное значение часа'
 
-grayscale_image = ImageOps.grayscale(image)
-grayscale_image.save("grayscale_osen-park-skameika-autumn-fall-colors-park.jpg")
 
-
-
-
-
-import requests
-
-r = requests.get('https://ya.ru/')
-print(r)
-
-
-
-
-import numpy as np
-a = np.array([20, 30, 40, 50])
-b = np.arange(1, 5)
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a ** b)
-print(a % b)
+a = SuperDate(2024, 2, 22, 12)
+print(a.get_season())
+print(a.get_time_of_day())
 
 
 
 
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-phi = np.linspace(0, 2.*np.pi, 100)
-plt.plot(phi, np.sin(phi))
-plt.plot(phi, np.cos(phi))
-
-plt.show()
-plt.close()
