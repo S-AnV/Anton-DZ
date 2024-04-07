097fcf55f74cb6944346140d69268debfd3d07d8
user <antonsfv@gmail.com>
2024-04-07 20:39:54 +0700

    Домашнее задание по теме "Дата и время"

--- probe.py
+++ probe.py
@@ -2,43 +2,36 @@ from datetime import datetime
 
 
 class SuperDate(datetime):
-    def __init__(self, year, month, day, hour):
-        self._year = year
-        self._month = month
-        self._day = day
-        self._hour = hour
+
     def get_season(self):
         """
         возвращает сезон года (Summer, Autumn, Winter, Spring)
         :return:
         """
-        if 1 <= self._month <= 12:
-            if 3 <= self._month <= 5:
-                return 'Spring'
-            elif 6 <= self._month <= 8:
-                return 'Summer'
-            elif 9 <= self._month <= 11:
-                return 'Autumn'
-            else:
-                return 'Winter'
+        if 3 <= self.month <= 5:
+            return 'Spring'
+        elif 6 <= self.month <= 8:
+            return 'Summer'
+        elif 9 <= self.month <= 11:
+            return 'Autumn'
         else:
-            return 'Введен некорректный номер месяца'
+            return 'Winter'
+
 
     def get_time_of_day(self):
         """
         возвращает время суток (Morning: 6-12; Day: 12-18, Evening: 18-0, Night: 0-6)
         :return:
         """
-        if 6 <= self._hour <= 11:
+        if 6 <= self.hour <= 11:
             return 'Morning'
-        elif 12 <= self._hour <= 17:
+        elif 12 <= self.hour <= 17:
             return 'Day'
-        elif 18 <= self._hour <= 23:
+        elif 18 <= self.hour <= 23:
             return 'Evening'
-        elif 0 <= self._hour <= 5:
+        elif 0 <= self.hour <= 5:
             return 'Night'
-        else:
-            return 'Введено некорректное значение часа'
+
 
 
 a = SuperDate(2024, 2, 22, 12)
