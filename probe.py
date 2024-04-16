47ea2c2bf78e122558f6a6aab90cbe5573781db2
user <antonsfv@gmail.com>
2024-04-16 22:30:50 +0700

    Домашнее задание по теме "Создание объектов, обработка изображений"

--- probe.py
+++ probe.py
@@ -1,17 +1,30 @@
-import re
+import cv2
+import numpy as np
 
-def extract_image_links(html_text):
-    pattern = r"\s*https?://[\w./]+.[e-p]{3,4}\s*"
-    string = re.findall(pattern, html_text)
-    return string
+photo = np.zeros((600, 600, 3), dtype='uint8')
+
+# i=0
+# k=0
+# for _ in range(40):
+#     photo[0+i:30+i, :] = (50+k, 255-k/7, 240)
+#     i += 15
+#     k += 5
+
+# cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 230, (255, 200, 230), thickness=2)
+
+cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 230, (255, 200, 230), thickness=3)
+
+cv2.ellipse(photo, center=(photo.shape[1] // 2, 330), axes=(30, 30), angle=0, startAngle=0, endAngle=180, color=(0, 255, 0), thickness=3)
+cv2.ellipse(photo, center=(photo.shape[1] // 2, 330), axes=(70, 70), angle=0, startAngle=0, endAngle=180, color=(0, 255, 0), thickness=3)
+
+cv2.line(photo, (photo.shape[1] // 2 - 30, 330), (photo.shape[1] // 2 - 30, 250), (255, 0, 0), thickness=3)
+cv2.line(photo, (photo.shape[1] // 2 - 70, 330), (photo.shape[1] // 2 - 70, 250), (255, 0, 0), thickness=3)
+cv2.line(photo, (photo.shape[1] // 2 + 30, 330), (photo.shape[1] // 2 + 30, 250), (255, 0, 0), thickness=3)
+cv2.line(photo, (photo.shape[1] // 2 + 70, 330), (photo.shape[1] // 2 + 70, 250), (255, 0, 0), thickness=3)
+
+cv2.ellipse(photo, center=(photo.shape[1] // 2 - 50, 250), axes=(20, 20), angle=180, startAngle=0, endAngle=180, color=(0, 0, 255), thickness=3)
+cv2.ellipse(photo, center=(photo.shape[1] // 2 + 50, 250), axes=(20, 20), angle=180, startAngle=0, endAngle=180, color=(0, 0, 255), thickness=3)
 
 
-sample_html = ("<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> "
-               "<img src='https://example.com/image3.gif'>")
-
-image_links = extract_image_links(sample_html)
-if image_links:
-  for image_link in image_links:
-    print(image_link)
-else:
-  print("Нет ссылок с картинками в HTML тексте.")
+cv2.imshow('Photo', photo)
+cv2.waitKey(0)
