49c96301949145a77e2d4cecc91443b81354a1a6
user <antonsfv@gmail.com>
2024-04-19 18:28:58 +0700

    Домашнее задание по теме "Создание объектов, обработка изображений"

--- probe.py
+++ probe.py
@@ -3,14 +3,14 @@ import numpy as np
 
 photo = np.zeros((600, 600, 3), dtype='uint8')
 
-# i=0
-# k=0
-# for _ in range(40):
-#     photo[0+i:30+i, :] = (50+k, 255-k/7, 240)
-#     i += 15
-#     k += 5
+i = 0
+k = 0
+for _ in range(600):
+    # photo[:, 0+i:30+i] = (50+k, 255-k/7, 240)
+    photo[:, 0 + i:1 + i] = (0 + k, 0 + k, 0 + k)
+    i += 1
+    k += 1 / 2.4
 
-# cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 230, (255, 200, 230), thickness=2)
 
 cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 230, (255, 200, 230), thickness=3)
 
@@ -28,3 +28,4 @@ cv2.ellipse(photo, center=(photo.shape[1] // 2 + 50, 250), axes=(20, 20), angle=
 
 cv2.imshow('Photo', photo)
 cv2.waitKey(0)
+
